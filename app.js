11466d2fd629bbf2afae6e2f23ef3cc0b8c8b71f
Laura Evans <laura.evans@ivieinc.com>
2015-07-18 14:28:31 -0500

    add basic GalleryMarker class

--- app.js
+++ app.js
@@ -36,8 +36,13 @@ function initialize() {
 	    ]
 	  }
 	];
+	
 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 	map.setOptions({styles: styles});
+	
+	overlay = new CustomMarker( myLatlng, map, { marker_id: 'my-marker' } );
+	overlay2 = new CustomMarker( new google.maps.LatLng(43.769560, 11.255814), map, { marker_id: 'my-marker2' } );
 
 }
+				
 google.maps.event.addDomListener(window, 'load', initialize);
